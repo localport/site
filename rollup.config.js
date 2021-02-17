d24e53ff6b15859584986bd4190c36824de08dba
battal <battal51280@gmail.com>
2021-02-17 19:30:14 +0300

    seperated geist-ui 2

--- rollup.config.js
+++ rollup.config.js
@@ -3,22 +3,18 @@ import path from "path";
 import config from "sapper/config/rollup.js";
 import pkg from "./package.json";
 
-import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
 import url from "@rollup/plugin-url";
-import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
+import svelte from "rollup-plugin-svelte";
 
 import sveltePreprocess from "svelte-preprocess";
 const preprocess = sveltePreprocess({
-	scss: {
-		includePaths: ["src"],
-	},
-	postcss: {
-		plugins: [require("autoprefixer")],
-	},
+  scss: { includePaths: ["src"] },
+  postcss: { plugins: [require("autoprefixer")] },
 });
 
 const mode = process.env.NODE_ENV;
@@ -26,103 +22,100 @@ const dev = mode === "development";
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-	(warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
-	(warning.code === "CIRCULAR_DEPENDENCY" &&
-		/[/\\]@sapper[/\\]/.test(warning.message)) ||
-	onwarn(warning);
+  (warning.code === "MISSING_EXPORT" && /'preload'/.test(warning.message)) ||
+  (warning.code === "CIRCULAR_DEPENDENCY" &&
+    /[/\\]@sapper[/\\]/.test(warning.message)) ||
+  onwarn(warning);
 
 export default {
-	client: {
-		input: config.client.input(),
-		output: config.client.output(),
-		plugins: [
-			replace({
-				"process.browser": true,
-				"process.env.NODE_ENV": JSON.stringify(mode),
-			}),
-			svelte({
-				compilerOptions: {
-					dev,
-					hydratable: true,
-				},
-				preprocess,
-			}),
-			url({
-				sourceDir: path.resolve(__dirname, "src/node_modules/images"),
-				publicPath: "/client/",
-			}),
-			resolve({
-				browser: true,
-				dedupe: ["svelte"],
-			}),
-			commonjs(),
+  client: {
+    input: config.client.input(),
+    output: config.client.output(),
+    plugins: [
+      replace({
+        "process.browser": true,
+        "process.env.NODE_ENV": JSON.stringify(mode),
+      }),
+      svelte({
+        compilerOptions: {
+          dev,
+          hydratable: true,
+        },
+        preprocess,
+      }),
+      url({
+        sourceDir: path.resolve(__dirname, "src/node_modules/images"),
+        publicPath: "/client/",
+      }),
+      resolve({
+        browser: true,
+        dedupe: ["svelte"],
+      }),
+      commonjs(),
 
-			legacy &&
-				babel({
-					extensions: [".js", ".mjs", ".html", ".svelte"],
-					babelHelpers: "runtime",
-					exclude: ["node_modules/@babel/**"],
-					presets: [
-						[
-							"@babel/preset-env",
-							{
-								targets: "> 0.25%, not dead",
-							},
-						],
-					],
-					plugins: [
-						"@babel/plugin-syntax-dynamic-import",
-						[
-							"@babel/plugin-transform-runtime",
-							{
-								useESModules: true,
-							},
-						],
-					],
-				}),
+      legacy &&
+        babel({
+          extensions: [".js", ".mjs", ".html", ".svelte"],
+          babelHelpers: "runtime",
+          exclude: ["node_modules/@babel/**"],
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                targets: "> 0.25%, not dead",
+              },
+            ],
+          ],
+          plugins: [
+            "@babel/plugin-syntax-dynamic-import",
+            [
+              "@babel/plugin-transform-runtime",
+              {
+                useESModules: true,
+              },
+            ],
+          ],
+        }),
 
-			!dev &&
-				terser({
-					module: true,
-				}),
-		],
+      !dev && terser({ module: true }),
+    ],
 
-		preserveEntrySignatures: false,
-		onwarn,
-	},
+    preserveEntrySignatures: false,
+    onwarn,
+  },
 
-	server: {
-		input: config.server.input(),
-		output: config.server.output(),
-		plugins: [
-			replace({
-				"process.browser": false,
-				"process.env.NODE_ENV": JSON.stringify(mode),
-			}),
-			svelte({
-				compilerOptions: {
-					dev,
-					generate: "ssr",
-					hydratable: true,
-				},
-				emitCss: false,
-				preprocess,
-			}),
-			url({
-				sourceDir: path.resolve(__dirname, "src/node_modules/images"),
-				publicPath: "/client/",
-				emitFiles: false, // already emitted by client build
-			}),
-			resolve({
-				dedupe: ["svelte"],
-			}),
-			commonjs(),
-		],
-		external: Object.keys(pkg.dependencies).concat(
-			require("module").builtinModules
-		),
+  server: {
+    input: config.server.input(),
+    output: config.server.output(),
+    plugins: [
+      replace({
+        "process.browser": false,
+        "process.env.NODE_ENV": JSON.stringify(mode),
+      }),
+      svelte({
+        compilerOptions: {
+          dev,
+          generate: "ssr",
+          hydratable: true,
+        },
+        emitCss: false,
+        preprocess,
+      }),
+      url({
+        sourceDir: path.resolve(__dirname, "src/node_modules/images"),
+        publicPath: "/client/",
+        emitFiles: false, // already emitted by client build
+      }),
+      resolve({
+        dedupe: ["svelte"],
+      }),
+      commonjs(),
+    ],
+    external: Object.keys(pkg.dependencies).concat(
+      require("module").builtinModules
+    ),
 
-		preserveEntrySignatures: "strict",
-		onwarn,
-	}
+    preserveEntrySignatures: "strict",
+    onwarn,
+  },
 };
