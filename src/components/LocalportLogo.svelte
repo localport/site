7cf8e05cfbc4c475d3804b3bc9483fe09cf2ffd9
battal <battal51280@gmail.com>
2021-02-18 23:55:12 +0300

    added logo

---
+++ src/components/LocalportLogo.svelte
@@ -0,0 +1,26 @@
+<script>
+  export let color = "currentColor";
+  export let size = 24;
+</script>
+
+<svg
+  stroke="currentColor"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  stroke-width="0.1"
+  shape-rendering="geometricPrecision"
+  viewBox="0 0 64 64"
+  {...$$restProps}
+  height={size}
+  width={size}
+  style="color: {color};"
+  fill={color}
+>
+  <g>
+    <path
+      d="m18,35.01405l11,0l0,6.43a11,11 0 0 0 -7.57,7.57l-17.43,0l0,6l17.43,0a11,11 0 0 0 21.15,0l16.42,0l0,-6l-16.43,0a11,11 0 0 0 -7.57,-7.57l0,-6.43l11,0a17,17 0 0 0 0,-34l-28,0a17,17 0 0 0 0,34zm19,17a5,5 0 1 1 -5,-5a5,5 0 0 1 5,5zm-19,-45l28,0a11,11 0 0 1 0,22l-28,0a11,11 0 0 1 0,-22z"
+    />
+    <circle r="3" cy="18.01405" cx="19" />
+    <circle r="3" cy="18.01405" cx="31" />
+  </g>
+</svg>
