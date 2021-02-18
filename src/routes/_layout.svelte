f90d00efbd8933e7dc79a11bc64a7f16ff3ec923
battal <battal51280@gmail.com>
2021-02-18 23:56:29 +0300

    storing theme in localstorage

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,24 +1,52 @@
 <script>
-  import "../style/global.css";
+  export let segment;
+  let isMenuOpen = false;
 
-  import Nav from "../components/Nav.svelte";
-
+  import { onMount, onDestroy } from "svelte";
   import { stores } from "@sapper/app";
   const { session } = stores();
 
-  import { onMount } from "svelte";
-  onMount(async () => {
-    $session = { who: "layout" };
-  });
+  import "geist-ui/dist/geist-ui.css";
+  import Nav from "../components/Nav.svelte";
+  import Menu from "../components/Menu.svelte";
+  import Footer from "../components/Footer.svelte";
 
-  export let segment;
+  // Session Initialization
+  onMount(() => {
+    // https://github.com/ConsoleTVs/sswr for data fetching
+    // https://blog.marksauerutley.com/sapper-authenticated-sessions/
+
+    // THEME
+    // theme.stored == "", means "system" is selected
+    $session.theme = {
+      stored: window.localStorage.getItem("theme") || "", // returns null by default
+      system: window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : "light",
+    };
+    session.subscribe((data) => {
+      // save theme to localstorage so remember what user want
+      window.localStorage.setItem("theme", data.theme.stored);
+      document.documentElement.setAttribute(
+        "data-theme",
+        !!data.theme.stored ? data.theme.stored : data.theme.system
+      );
+    });
+  });
 </script>
 
-<Nav {segment} />
+<Nav {segment} on:toggleMenu={() => (isMenuOpen = !isMenuOpen)} />
+<Menu
+  {segment}
+  isOpen={isMenuOpen}
+  on:changeTheme={(theme) => $session.theme.stored === theme}
+/>
 
 <main>
   <slot />
 </main>
+
+<Footer />
 
 <style lang="scss">
   @import "../style/sizes.scss";
